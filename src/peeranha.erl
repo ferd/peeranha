62f4900644e04a158e4b91ad9b2bbb236a7b65bb
Fred Hebert <mononcqc@ferd.ca>
2014-04-16 11:25:32 -0400

    Add a pull mechanism for pre/post hooks
    sync/2 is a push-pull mechanism (both pulls data from remote nodes and
    then pushes its own updates). This makes it hard to deal with pre/post
    hooks when they might be executing at once on multiple nodes, and the
    control flow would be very complex.
    
    To allow people to use pre and post hooks, a pull-only mechanism is
    introduced, which allows instances to do their fetching caring only
    about local node content. This implies that all node must pull from
    remote ones, and simulating a push/pull with pulls only can be done by
    looking at the diff result, sending a remote notification, etc.
    
    The pre and post hooks are basic: the pre hook will be notified of what
    the merge would look like (local wins, peer wins, or a conflict) along
    with the values from both sides, can do whatever it wants, and return
    `skip` or `ok` in order to cancel the pull for this value only, or keep
    it going.
    
    The post hook has no direct impact, and is therefore only informed of
    the key and final value. No state is carried from the pre to post hook
    at this time, and a post hook won't be called after a pre hook has
    decided to 'skip' a specific merge.

--- src/peeranha.erl
+++ src/peeranha.erl
@@ -1,5 +1,7 @@
+%%% TODO: - Deleted entries GC in merkle tree
+%%%       - allow to retire into another node
 -module(peeranha).
--export([boot/2, retire/1, fork/3, sync/2]).
+-export([boot/2, retire/1, fork/3, sync/2, pull/2, pull/4]).
 -export([read/2, peek/2, write/3, delete/2]).
 -export([sync/4, fork/1]).
 
@@ -22,44 +24,71 @@ fork(Remote, Name, Dir) ->
                          {type, normal}, {id, NewId}]).
 
 
-sync(Local, Remote) ->
-    {ok, Diff} = peeranha_sync:diff(Local, Remote),
+%% Push/pull mechanism
+sync(Local, Peer) ->
+    {ok, Diff} = peeranha_sync:diff(Local, Peer),
     Ops = [case interclock:peek(Local, Key) of
             {error, undefined} ->
                 %% Fetch the value from a peer node // Pull Anti-Entropy
-                {ok, Event, Vals} = peeranha_peer:peek(Remote, Key),
+                {ok, Event, Vals} = peeranha_peer:peek(Peer, Key),
                 Res = interclock:sync(Local, Key, Event, Vals),
                 %% Update our sync tree
                 TreeVal = peeranha:peek(Local, Key),
                 peeranha_sync:write(Local, Key, TreeVal),
                 %% Build the diff list
                 {Res, Key};
-            _ ->
-                %% We have a value, but maybe our peer doesn't // Push-Pull
-                {ok, Event, Vals} = interclock:peek(Local, Key),
-                peeranha_peer:sync(Remote, Key, Event, Vals), % can optimize based on result
+            {ok, Event, Vals} ->
+                %% We have a value, but maybe our peer doesn't // Push-Pull Anti-Entropy
+                peeranha_peer:sync(Peer, Key, Event, Vals), % can optimize based on result
                 %% Read back and merge locally
-                {ok, MergedEvent, MergedVals} = peeranha_peer:peek(Remote, Key),
+                {ok, MergedEvent, MergedVals} = peeranha_peer:peek(Peer, Key),
                 Res = interclock:sync(Local, Key, MergedEvent, MergedVals),
                 %% Update the sync tree
                 TreeVal = peeranha:peek(Local, Key),
                 peeranha_sync:write(Local, Key, TreeVal),
-                %% Build the diff list
-                case peeranha:read(Local, Key) of
-                    {conflict, _} ->
-                        {conflict, Key};
-                    {conflict, deleted, _} ->
-                        {conflict, Key};
-                    _ ->
-                        {Res, Key}
-                end
+                to_diff(Local, Key, Res)
            end || Key <- Diff],
      lists:foldr(fun({conflict,Key}, {Merge,Conflict}) -> {Merge, [Key|Conflict]}
                  ;  ({_, Key}, {Merge,Conflict}) -> {[Key|Merge], Conflict} end,
                  {[],[]},
                  Ops).
 
-%% TODO: allow to retire into another node
+pull(Local, Remote) ->
+    Pre = fun(_Winner, _Key, _LocalVals, _RemoteVals) -> ok end,
+    Post = fun(_Key, _Vals) -> ok end,
+    pull(Local, Remote, Pre, Post).
+
+-spec pull(Local, Peer, Pre, Post) -> Result when
+    Local :: term(),
+    Peer :: term(),
+    Pre :: fun((Winner, Key, LocalVal, PeerVal) -> ok | skip),
+    Post :: fun((Key, Val) -> ok),
+    Winner :: peer | local | conflict,
+    Key :: binary(),
+    LocalVal :: Val,
+    PeerVal :: Val,
+    Val :: undefined | {ok, term()} | {conflict, [term()]} | {conflict, deleted, [term()]},
+    Result :: {Merged::[Key], Conflicted::[Key], Skipped::[Key]}.
+pull(Local, Peer, Pre, Post) ->
+    {ok, Diff} = peeranha_sync:diff(Local, Peer),
+    Ops = [case interclock:peek(Local, Key) of
+            {error, undefined} -> % peer owns it
+                {ok, PeerEvent, PeerVals} = peeranha_peer:peek(Peer, Key),
+                pull_merge(Local, Key, Pre, Post, undefined, PeerEvent, PeerVals);
+            {ok, _LocalEvent, LocalVals} -> % we have a copy
+                case peeranha_peer:peek(Peer, Key) of
+                    {error, undefined} -> % we own it
+                        {skip, Key};
+                    {ok, PeerEvent, PeerVals} ->
+                        pull_merge(Local, Key, Pre, Post, LocalVals, PeerEvent, PeerVals)
+                end
+           end || Key <- Diff],
+     lists:foldr(fun({conflict,Key}, {Merge,Conflict,Skip}) -> {Merge, [Key|Conflict], Skip}
+                 ;  ({skip, Key}, {Merge,Conflict,Skip}) -> {Merge, Conflict, [Key|Skip]}
+                 ;  ({_, Key}, {Merge,Conflict,Skip}) -> {[Key|Merge], Conflict, Skip} end,
+                 {[],[],[]},
+                 Ops).
+
 retire(Name) ->
     peeranha_sync:retire(Name),
     interclock:retire(Name).
@@ -96,4 +125,38 @@ sync(Name, Key, Event, Val) ->
 
 fork(Name) ->
     interclock:fork(Name).
+
+%%%%%%%%%%%%%%%
+%%% PRIVATE %%%
+%%%%%%%%%%%%%%%
+
+pull_merge(Local, Key, Pre, Post, LocalVals, PeerEvent, PeerVals) ->
+    Win = interclock:simulate_sync(Local, Key, PeerEvent, PeerVals),
+    case Pre(Win, Key, vals_to_hook(LocalVals), vals_to_hook(PeerVals)) of
+        skip ->
+            {skip, Key};
+        ok ->
+            Win = interclock:sync(Local, Key, PeerEvent, PeerVals),
+            %% Update the sync tree
+            TreeVal = {ok,_,FinalVal} = peeranha:peek(Local, Key),
+            peeranha_sync:write(Local, Key, TreeVal),
+            Post(Key, vals_to_hook(FinalVal)),
+            to_diff(Local, Key, Win)
+    end.
+
+vals_to_hook(undefined) -> undefined;
+vals_to_hook([Val]) -> {ok, Val};
+vals_to_hook(Vals=[_|_]) -> {conflict, Vals};
+vals_to_hook({deleted, _, []}) -> undefined;
+vals_to_hook({deleted, _, Vals}) -> {conflict, deleted, Vals}.
+
+to_diff(Name, Key, Res) ->
+    case peeranha:read(Name, Key) of
+        {conflict, _} ->
+            {conflict, Key};
+        {conflict, deleted, _} ->
+            {conflict, Key};
+        _ ->
+            {Res, Key}
+    end.
 
--- test/peeranha_sync_SUITE.erl
+++ test/peeranha_sync_SUITE.erl
@@ -10,7 +10,8 @@
 %%%===================================================================
 
 all() ->
-    [build, diff1, diff2, diff3]. % to add: stop diff
+    [build, diff1, diff2, diff3,
+     pull1, pull2].
 
 init_per_testcase(build, Config) ->
     {ok, Started} = application:ensure_all_started(peeranha),
@@ -133,4 +134,110 @@ diff3(Config) ->
     {error, undefined} = peeranha:read(Name, <<"key1">>),
     {error, undefined} = peeranha:read(Name, <<"key2">>),
     {error, undefined} = peeranha:read(Name, <<"key3">>).
+
+pull1(Config) ->
+    %% Create a bunch of entries. One is the same on both
+    %% nodes (albeit with a different clock), two are
+    %% different (one on each) and one conflicts.
+    Name = ?config(name, Config),
+    NameFork = ?config(name_fork, Config),
+    peeranha:write(Name, <<"key1">>, val),
+    peeranha:write(Name, <<"key2">>, val),
+    peeranha:write(Name, <<"key4">>, val),
+    peeranha:write(NameFork, <<"key1">>, val),
+    peeranha:write(NameFork, <<"key3">>, val),
+    peeranha:write(NameFork, <<"key4">>, otherval),
+    %% Pulling takes place
+    Pre = fun(Win, Key, LocVal, PeerVal) ->
+            ct:pal("Pre: ~p", [{Win,Key,LocVal,PeerVal}])
+    end,
+    Post = fun(Key, Final) ->
+            ct:pal("Post: ~p", [{Key,Final}])
+    end,
+    Pull = peeranha:pull(Name, {peeranha_erldist, {node(), NameFork}}, Pre, Post),
+    {[<<"key1">>,<<"key3">>],[<<"key4">>],[<<"key2">>]} = Pull,
+    {ok, val} = peeranha:read(Name, <<"key1">>),
+    {ok, val} = peeranha:read(Name, <<"key2">>),
+    {ok, val} = peeranha:read(Name, <<"key3">>),
+    {conflict, Conflict} = peeranha:read(Name, <<"key4">>),
+    ?assert(lists:member(val, Conflict)),
+    ?assert(lists:member(otherval, Conflict)),
+    %% Nothing needs to be pulled back, but we still end up re-comparing
+    %% all keys because the other end has not sync'd back. Because this is
+    %% a pull mechanism instead of a push/pull like sync/2, this will happen
+    %% until the other node has pulled from this one.
+    {[<<"key1">>],[<<"key4">>],[<<"key2">>]} = peeranha:pull(Name, {peeranha_erldist, {node(), NameFork}},Pre,Post),
+    {ok, val} = peeranha:read(Name, <<"key1">>),
+    {ok, val} = peeranha:read(Name, <<"key2">>),
+    {ok, val} = peeranha:read(Name, <<"key3">>),
+    {conflict, Conflict} = peeranha:read(Name, <<"key4">>),
+    ?assert(lists:member(val, Conflict)),
+    ?assert(lists:member(otherval, Conflict)),
+    %% Crush the conflict
+    peeranha:write(Name, <<"key4">>, crushed),
+    %% this one pull is also done without hooks -- should have no impact
+    {[<<"key1">>,<<"key4">>],[],[<<"key2">>]} = peeranha:pull(Name, {peeranha_erldist, {node(), NameFork}}),
+    {ok, crushed} = peeranha:read(Name, <<"key4">>),
+    %% Pull from the remote node and the conflicts should go away
+    {[<<"key1">>,<<"key2">>,<<"key4">>],[],[]} = peeranha:pull(NameFork, {peeranha_erldist, {node(), Name}},Pre,Post),
+    {[],[],[]} = peeranha:pull(Name, {peeranha_erldist, {node(), NameFork}},Pre,Post).
+
+pull2(Config) ->
+    %% Create a bunch of entries. Two are the same on both
+    %% nodes (albeit with a different clock), one is
+    %% different (on the peer) and one conflicts.
+    Name = ?config(name, Config),
+    NameFork = ?config(name_fork, Config),
+    peeranha:write(Name, <<"key1">>, val),
+    peeranha:write(Name, <<"key2">>, val),
+    peeranha:write(Name, <<"key4">>, val),
+    peeranha:write(NameFork, <<"key1">>, val),
+    peeranha:write(NameFork, <<"key2">>, val),
+    peeranha:write(NameFork, <<"key3">>, val),
+    peeranha:write(NameFork, <<"key4">>, otherval),
+    peeranha:write(NameFork, <<"key5">>, otherval),
+    %% Pulling takes place
+    Pre = fun(conflict, _Key, _LocVal, _PeerVal) -> skip
+          ;  (peer, _Key, _LocVal, {ok,otherval}) -> skip
+          ;  (peer, _Key, _LocVal, _PeerVal) -> ok
+          ;  (local, <<"key1">>, _LocVal, _PeerVal) -> skip
+          ;  (local, <<"key2">>, _LocVal, _PeerVal) -> ok
+    end,
+    Post = fun(Key, Final) ->
+            ct:pal("Post: ~p", [{Key,Final}])
+    end,
+    Pull = peeranha:pull(Name, {peeranha_erldist, {node(), NameFork}}, Pre, Post),
+    {[<<"key2">>,<<"key3">>],[],[<<"key1">>,<<"key4">>,<<"key5">>]} = Pull,
+    {ok, val} = peeranha:read(Name, <<"key1">>),
+    {ok, val} = peeranha:read(Name, <<"key2">>),
+    {ok, val} = peeranha:read(Name, <<"key3">>),
+    {ok, val} = peeranha:read(Name, <<"key4">>),
+    {error, undefined} = peeranha:read(Name, <<"key5">>),
+    {_,[<<"key4">>],_} = peeranha:pull(Name, {peeranha_erldist, {node(), NameFork}}),
+    {conflict, Conflict} = peeranha:read(Name, <<"key4">>),
+    ?assert(lists:member(val, Conflict)),
+    ?assert(lists:member(otherval, Conflict)),
+    Pre2 = fun(local, <<"key4">>, {conflict,[_,_]}, {ok,otherval}) -> skip
+           ;  (_, Key, _, _) when Key =/= <<"key4">> -> skip
+           ;  (A, B, C, D) -> error({A,B,C,D})
+    end,
+    peeranha:pull(Name, {peeranha_erldist, {node(), NameFork}}, Pre2, Post),
+    peeranha:delete(NameFork, <<"key4">>),
+    Pre3 = fun(conflict, <<"key4">>, {conflict,[_,_]}, undefined) -> ok
+           ;  (_, Key, _, _) when Key =/= <<"key4">> -> skip
+           ;  (A, B, C, D) -> error({A,B,C,D})
+    end,
+    {_,[<<"key4">>],_} = peeranha:pull(Name, {peeranha_erldist, {node(), NameFork}}, Pre3, Post),
+    Pre4 = fun(local, <<"key4">>, {conflict,deleted,[_,_]}, undefined) -> ok
+           ;  (_, Key, _, _) when Key =/= <<"key4">> -> skip
+           ;  (A, B, C, D) -> error({A,B,C,D})
+    end,
+    %% Even though the local value is higher, this remains a conflict
+    {_,[<<"key4">>],_} = peeranha:pull(Name, {peeranha_erldist, {node(), NameFork}}, Pre4, Post),
+    %% Remote pull
+    Pre5 = fun(peer, <<"key4">>, undefined, {conflict,deleted,[_,_]}) -> ok
+           ;  (_, Key, _, _) when Key =/= <<"key4">> -> skip
+           ;  (A, B, C, D) -> error({A,B,C,D})
+    end,
+    {_,[<<"key4">>],_} = peeranha:pull(NameFork, {peeranha_erldist, {node(), Name}}, Pre5, Post).
 
